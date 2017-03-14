a2f30e6ad6ebec6347c5215410112819f065df9a
Essi Suurkuukka <essi.suurkuukka@dreamloop.net>
2017-03-14 16:36:16 +0200

    GameManager and NetworkManagerCustom

---
+++ Unity/ProjectEhe/Assets/Scripts/GameManager.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Scripts
+{
+    public class GameManager : MonoBehaviour
+    {
+        public List<PlayerMovement> PlayerMovements = new List<PlayerMovement>();
+
+        public struct PlayerAction
+        {
+            public int Id;
+            public Enumerations.Action Action;
+        }
+
+        public class PlayerActions : SyncListStruct<PlayerAction> {}
+        PlayerActions m_actions = new PlayerActions();
+
+        //public Dictionary<int, List<Enumerations.Action>> PlayerActions = new Dictionary<int, List<Enumerations.Action>>();
+
+        public NetworkManagerCustom NetworkManager;
+
+        public int MaxActionAmount = 5;
+        public float TurnInterval = 3.0f;
+
+        private float turnTimeLeft;
+
+        public bool TurnRunning;
+
+        void Awake()
+        {
+           NetworkManager.PlayerAdded += NetworkManagerOnPlayerAdded;
+            m_actions.Callback += OnPlayerActionChanged;
+        }
+
+        private void OnPlayerActionChanged(SyncList<PlayerAction>.Operation op, int itemIndex)
+        {
+            Debug.Log("Player action changed: " + op);
+        }
+
+        void Update()
+        {
+            if (TurnRunning)
+            {
+                turnTimeLeft -= Time.deltaTime;
+                if (turnTimeLeft < 0)
+                {
+                    DoNextTurnAction();
+                    ResetTimer();
+                    turnTimeLeft = 0;
+                }
+            }
+        }
+
+        void ResetTimer()
+        {
+            turnTimeLeft = TurnInterval;
+        }
+
+        private void NetworkManagerOnPlayerAdded(int hostId)
+        {
+            Debug.Log("HostId: " + hostId);
+
+            if (PlayerMovements.All(item => item.connectionToClient.hostId != hostId))
+            {
+                var playerMovements = GameObject.FindObjectsOfType<PlayerMovement>();
+
+                foreach (var playerMovement in playerMovements)
+                {
+                    Debug.Log("PlayerMovement hostId: " + playerMovement.connectionToClient.hostId);
+                }
+
+                if (playerMovements.Any(item => item.connectionToClient.hostId == hostId))
+                {
+                    var plrMovement = playerMovements.First(item => item.connectionToClient.hostId == hostId);
+                    PlayerMovements.Add(plrMovement);
+                    playerMovements.First(item => item.connectionToClient.hostId == hostId).Id =
+                        plrMovement.connectionToClient.hostId;
+                    //plrMovement.PlayerActionGiven += PlayerActionGiven;
+                    //PlayerActions.Add(plrMovement.Id, new List<Enumerations.Action>());
+
+                }
+            }
+
+            if (PlayerMovements.Count == 2)
+                StartCoroutine(WaitAndStartGame());
+        }
+
+        IEnumerator WaitAndStartGame()
+        {
+            Debug.Log("Waiting and starting the game");
+            yield return new  WaitForSeconds(3F);
+
+            Debug.Log("Starting the game...");
+
+            StartGame();
+        }
+
+        public void PlayerActionGiven(int hostId, Enumerations.Action action)
+        {
+            Debug.Log("Player action given : " + hostId + " " + action);
+
+            //foreach (var thing in PlayerActions)
+            //{
+            //    Debug.Log(thing.Key);
+            //}
+
+            //if (PlayerActions[hostId].Count < MaxActionAmount)
+            //{
+            //    PlayerActions[hostId].Add(action);
+            //}
+
+            //if (PlayerActions.All(item => item.Value.Count == 5))
+            //{
+            //    StartTurnExecuting();
+            //}
+
+            foreach (var thing in m_actions)
+            {
+                Debug.Log(thing.Id + " " + thing.Action);
+            }
+
+            if (m_actions.Count(item => item.Id == hostId) < MaxActionAmount)
+            {
+                m_actions.Add(new PlayerAction() { Action = action, Id = hostId});
+            }
+
+            //if (PlayerActions.All(item => item.Value.Count == 5))
+            //{
+            //    StartTurnExecuting();
+            //}
+        }
+
+        void StartGame()
+        {
+            foreach (var playerMovement in PlayerMovements)
+            {
+                playerMovement.AskForInput();
+            }
+        }
+
+        public void StartTurnExecuting()
+        {
+            TurnRunning = true;
+            turnTimeLeft = TurnInterval;
+        }
+
+        private void DoNextTurnAction()
+        {
+            //foreach (var hostId in PlayerActions.Keys)
+            //{
+            //    if (PlayerActions[hostId].Any())
+            //    {
+            //        PlayerMovements.First(item => item.Id == hostId).DoAction(PlayerActions[hostId].First());
+            //        PlayerActions[hostId].RemoveAt(0);
+            //    }
+            //}
+        }
+    }
+}
---
+++ Unity/ProjectEhe/Assets/Scripts/NetworkManagerCustom.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+ 
+public class NetworkManagerCustom : NetworkManager
+{
+    public delegate void PlayerAddedEventHandler(int connId);
+
+    public event PlayerAddedEventHandler PlayerAdded;
+
+    //When a new client connect to the Host server.
+    public override void OnClientConnect(NetworkConnection Conn)
+    {
+        base.OnClientConnect(Conn);
+
+        Debug.Log("Client has connected " + Conn.hostId);
+    }
+
+    //When a new client connect to the Host server.
+    public override void OnServerConnect(NetworkConnection Conn){
+
+        base.OnServerConnect(Conn);
+
+        Debug.Log("New Player has joined" + Conn.hostId);
+
+        //if(PlayerAdded != null)
+        //    PlayerAdded(Conn.hostId);
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        base.OnServerAddPlayer(conn, playerControllerId);
+
+        Debug.Log("PlayerControllerId " + playerControllerId);
+
+        if (PlayerAdded != null)
+            PlayerAdded(conn.hostId);
+    }
+}
